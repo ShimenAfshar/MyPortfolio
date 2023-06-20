285bd647406ecb7e0b46de328181a4fe7ae7e46b
ShimenAfshar <afsharshimen@gmail.com>
2023-06-20 20:08:53 +0100

    import pages into App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./index.css";
+import Home from "./routes/Home";
+import Project from "./routes/Project";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
 
 function App() {
   return (
     <div>
       <h1>this is the app.js</h1>
-      
     </div>
   );
 }
--- my-react-app/src/routes/About.js
+++ my-react-app/src/routes/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Project = () => {
+const About = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Project = () => {
   )
 }
 
-export default Project
+export default About
