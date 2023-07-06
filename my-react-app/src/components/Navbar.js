b55e61a80f6df464e1b26599606cd420ae8a31ff
ShimenAfshar <afsharshimen@gmail.com>
2023-07-06 19:53:28 +0100

    transition -navBar

--- my-react-app/src/components/Navbar.js
+++ my-react-app/src/components/Navbar.js
@@ -18,7 +18,7 @@ const Nabar = () => {
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
-      <ul className="nav-menu">
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li>
           <Link to="/">Home</Link>
         </li>
