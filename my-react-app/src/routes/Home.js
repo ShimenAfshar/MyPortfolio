130ca00f1fd95a6e013e1d2080776596632d272e
ShimenAfshar <afsharshimen@gmail.com>
2023-07-06 20:03:53 +0100

    HeroImg-component

--- my-react-app/src/routes/Home.js
+++ my-react-app/src/routes/Home.js
@@ -1,10 +1,13 @@
+/* eslint-disable react/jsx-no-undef */
 import React from 'react'
 import Navbar from '../components/Navbar';
+import HeroImg from '../components/HeroImg';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg />
     </div>
   )
 }
