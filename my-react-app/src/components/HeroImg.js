e1cf791a37696f4112e79cd336cc90f8664bceda
ShimenAfshar <afsharshimen@gmail.com>
2023-07-09 16:25:35 +0100

    btns-main page

--- my-react-app/src/components/HeroImg.js
+++ my-react-app/src/components/HeroImg.js
@@ -4,13 +4,22 @@ import "./HeroImgStyles.css";
 import React from 'react'
 
 import IntroImg from "../assets/heroimg.jpg";
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
     <div className="hero">
       <div className="mask">
               <img className="intro-img" src={IntroImg} alt="IntroImg"/>
+      </div>
+      <div className="content">
+        <p>Hi, I'm Shimen Afshar</p>
+        <h1>Full-Stack Developer</h1>
+        <div>
+            <Link to="/project" className="btn">Projects</Link>
+            <Link to="/contact" className="btn btn-light">Contact</Link>
 
+        </div>
       </div>
     </div>
   )
