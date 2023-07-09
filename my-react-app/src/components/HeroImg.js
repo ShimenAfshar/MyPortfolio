29bc4e7177be844807238dad72e2177a1792d8c5
ShimenAfshar <afsharshimen@gmail.com>
2023-07-09 16:12:11 +0100

    update-background-pic

--- my-react-app/src/components/HeroImg.js
+++ my-react-app/src/components/HeroImg.js
@@ -1,12 +1,15 @@
+
 import "./HeroImgStyles.css";
 
 import React from 'react'
+
+import IntroImg from "../assets/heroimg.jpg";
 
 const HeroImg = () => {
   return (
     <div className="hero">
       <div className="mask">
-        
+              <img className="intro-img" src={IntroImg} alt="IntroImg"/>
 
       </div>
     </div>
