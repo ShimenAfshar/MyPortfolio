2f78e1f205be7eec8d9a7bd96a3cd9c4a78c7d31
ShimenAfshar <afsharshimen@gmail.com>
2023-07-30 15:49:14 +0100

    heroImg2-text-style

--- my-react-app/src/components/HeroImg2.js
+++ my-react-app/src/components/HeroImg2.js
@@ -5,7 +5,8 @@ import React from "react";
 const HeroImg2 = () => {
   return ( <div className="hero-img">
     <div className="heading">
-
+        <h1>This is a heading</h1>
+        <p>This is a text</p>
     </div>
     </div>
   );
