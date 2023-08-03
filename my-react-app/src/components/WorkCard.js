072c7df8d256b65f781f4e4da7f55729be35f113
ShimenAfshar <afsharshimen@gmail.com>
2023-08-03 20:26:57 +0100

    final project

--- my-react-app/src/components/WorkCard.js
+++ my-react-app/src/components/WorkCard.js
@@ -2,12 +2,24 @@ import "./WorkCardStyles.css";
 
 import React from "react";
 
+import pro1 from "../assets/finalll.jpg";
+import { NavLink } from "react-router-dom";
+
 const WorkCard = () => {
   return (
     <div className="work-container">
         <h1 className="project-heading">Projects</h1>
         <div className="project-container">
             <div className="project-card">
+                <img src={pro1} alt="image"/>
+                <h2 className="project-title">Project's Title</h2>
+                <div className="pro-detailes">
+                      <p>this is a text</p>
+                      <div className="pro-btns">
+                        <NavLink to="url.com" className="btn">View</NavLink>
+
+                      </div>
+                </div>
 
             </div>
 
