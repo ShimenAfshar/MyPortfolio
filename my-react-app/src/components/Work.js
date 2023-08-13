01edcb7ae7bafed238cfeabd1e7f2fcbef9ab57f
ShimenAfshar <afsharshimen@gmail.com>
2023-08-13 13:27:56 +0100

    first card- project

---
+++ my-react-app/src/components/Work.js
@@ -0,0 +1,21 @@
+import "./WorkCardStyles.css";
+
+import React from "react";
+
+import pro1 from "../assets/finalll.jpg";
+import { NavLink } from "react-router-dom";
+
+const Work = () => {
+    return (
+        <div className="work-container">
+            <h1 className="project-heading">Projects</h1>
+            <div className="project-container">
+              
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Work;
