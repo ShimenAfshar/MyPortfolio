2a8256879bdd1b772b8d61a4adf3d065233b0356
ShimenAfshar <afsharshimen@gmail.com>
2023-08-14 19:42:11 +0100

    project cards

---
+++ my-react-app/src/components/WorkCardData.js
@@ -0,0 +1,12 @@
+
+import pro1 from "../assets/finalll.jpg"
+import pro2 from "../assets/mid.jpg"
+import pro3 from "../assets/hotel.jpg"
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "title of project",
+        text: "projects description"
+    }
+]
