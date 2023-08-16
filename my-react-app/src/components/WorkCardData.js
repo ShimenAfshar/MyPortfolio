d91264a733cab0d37118c0ec8c8e4c6d843f0e0d
ShimenAfshar <afsharshimen@gmail.com>
2023-08-16 20:12:23 +0100

    secend card props

--- my-react-app/src/components/WorkCardData.js
+++ my-react-app/src/components/WorkCardData.js
@@ -8,5 +8,18 @@ const ProjectCardData = [
         imgsrc: pro1,
         title: "Attendance Tracker",
         text: "Developed a class registration system to track attendance, ensuring accurate record-keeping and streamlined administrative processes."
-    }
+        view: "https://starter-kit-5h3s.onrender.com/"
+    },
+    {
+        imgsrc: pro2,
+        title: "Charities for Donations",
+        text: "Developed a website to identify verified charity organizations for making donations."
+        view: "https://buddy-group-project.netlify.app/"
+    },
+    {
+        imgsrc: pro3,
+        title: "Hotel booking application",
+        text: "Developed a class registration system to track attendance, ensuring accurate record-keeping and streamlined administrative processes."
+        view: "https://starter-kit-5h3s.onrender.com/"
+    },
 ]
