9450082dc82bf028b90998faeb6e02ca42d0b442
ShimenAfshar <afsharshimen@gmail.com>
2023-08-20 19:07:51 +0100

    ADD TEXT ON EACH CARD

--- my-react-app/src/components/WorkCardData.js
+++ my-react-app/src/components/WorkCardData.js
@@ -1,4 +1,3 @@
-
 import pro1 from "../assets/finalll.jpg";
 import pro2 from "../assets/mid.jpg";
 import pro3 from "../assets/hotel.jpg";
@@ -7,21 +6,21 @@ const ProjectCardData = [
     {
         imgsrc: pro1,
         title: "Attendance Tracker",
-        text: "Developed a class registration system to track attendance, ensuring accurate record-keeping and streamlined administrative processes."
+        text: "Developed a class registration system to track attendance, ensuring accurate record-keeping and streamlined administrative processes.",
         view: "https://starter-kit-5h3s.onrender.com/"
     },
     {
         imgsrc: pro2,
         title: "Charities for Donations",
-        text: "Developed a website to identify verified charity organizations for making donations."
+        text: "Developed a website to identify verified charity organizations for making donations.",
         view: "https://buddy-group-project.netlify.app/"
     },
     {
         imgsrc: pro3,
         title: "Hotel booking application",
-        text: "Created a hotel booking application using React involves several steps and components, including: Listing hotel bookings, Displaying the number of nights, Allowing users to order food and more"
+        text: "Created a hotel booking application that lists hotel bookings, displays the number of nights, and allows users to order food and more.",
         view: "https://dainty-dodol-4244d3.netlify.app/"
     },
-]
+];
 
 export default ProjectCardData;
