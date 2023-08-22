20c284c554149deb7cf401841be5d6be84302a30
ShimenAfshar <afsharshimen@gmail.com>
2023-08-22 19:04:56 +0100

    about component

---
+++ my-react-app/src/components/AboutContent.js
@@ -0,0 +1,15 @@
+
+import "./AboutContentStyles.css"
+
+import React from 'react'
+
+const AboutContent = () => {
+  return (
+    <div className="about">
+        
+      
+    </div>
+  )
+}
+
+export default AboutContent
--- my-react-app/src/routes/About.js
+++ my-react-app/src/routes/About.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
+import AboutContent from '../components/AboutContent';
 
 
 const About = () => {
@@ -10,6 +11,7 @@ const About = () => {
     <div>
       <Navbar />
       <HeroImg2 heading="ABOUT" text="I am a Full-Stack Developer"/>
+      <AboutContent />
       <Footer />
     </div>
   );
