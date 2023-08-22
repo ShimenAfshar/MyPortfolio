f6e359755b7050bbe70d1f72eda950ad47a1a42d
ShimenAfshar <afsharshimen@gmail.com>
2023-08-22 19:19:18 +0100

    import Imgs in about page

--- my-react-app/src/components/AboutContent.js
+++ my-react-app/src/components/AboutContent.js
@@ -1,4 +1,5 @@
 
+import { Link } from "react-router-dom"
 import "./AboutContentStyles.css"
 
 import React from 'react'
@@ -6,7 +7,20 @@ import React from 'react'
 const AboutContent = () => {
   return (
     <div className="about">
-        
+        <div className="left">
+            <p>After gaining valuable experience in the beauty industry, I embarked on an exciting journey in the tech world, driven by a desire for fresh challenges. By enrolling in Code Your Future's 10-month part-time full-stack bootcamp, I not only honed my skills but also expanded my knowledge. Now, I am actively seeking a company that nurtures a positive work culture and provides a supportive environment for growth. With unwavering motivation, I am dedicated to continuous personal and professional development.</p>
+            <Link TO="/contact">
+                <button className="btn">Contact</button>
+            </Link>
+        </div>
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack top">
+                    
+                </div>
+
+            </div>
+        </div>
       
     </div>
   )
