9caa947652f3646ae1cfa424e1a669cefba8c277
ShimenAfshar <afsharshimen@gmail.com>
2023-08-22 19:49:45 +0100

    update imgs

--- my-react-app/src/components/AboutContent.js
+++ my-react-app/src/components/AboutContent.js
@@ -4,6 +4,10 @@ import "./AboutContentStyles.css"
 
 import React from 'react'
 
+import React1 from "../assets/react1.jpeg"
+
+import React2 from "../assets/react2.webp"
+
 const AboutContent = () => {
   return (
     <div className="about">
@@ -16,8 +20,14 @@ const AboutContent = () => {
         <div className="right">
             <div className="img-container">
                 <div className="img-stack top">
-                    
+                    <img src={React1}
+                    className="img" alt="true"/>
                 </div>
+                  <div className="img-stack bottom">
+                      <img src={React2}
+                          className="img" alt="true" />
+
+                  </div>
 
             </div>
         </div>
