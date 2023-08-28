5b581e04dd5994e3912cbb4e922cbd4bb1e0e972
ShimenAfshar <afsharshimen@gmail.com>
2023-08-28 11:56:56 +0100

    form component-contact

--- my-react-app/src/routes/Contact.js
+++ my-react-app/src/routes/Contact.js
@@ -3,6 +3,7 @@ import React from "react"
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HeroImg2 from "../components/HeroImg2";
+import { Form } from "react-router-dom";
 
 
 const Contact = () => {
@@ -10,6 +11,7 @@ const Contact = () => {
     <div>
       <Navbar />
       <HeroImg2 heading="CONTACT" text="Get in Touch" />
+      <Form />
       <Footer />
     </div>
   );
